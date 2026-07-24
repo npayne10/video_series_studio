56b655bc8bf42afa0c79c0a81c2ec584c70de608
Neill Payne <npayne@g-host.co.za>
2026-07-24 16:21:23 +0200

    Sort CAP domain exports

--- src/vscs/domain/caps/__init__.py
+++ src/vscs/domain/caps/__init__.py
@@ -1,5 +1,5 @@
 """Canonical Asset Profile domain exports."""
 
-from vscs.domain.caps.models import CAPCreate, CAPStatus, CAPUpdate, CanonicalAssetProfile
+from vscs.domain.caps.models import CanonicalAssetProfile, CAPCreate, CAPStatus, CAPUpdate
 
 __all__ = ("CAPCreate", "CAPStatus", "CAPUpdate", "CanonicalAssetProfile")
