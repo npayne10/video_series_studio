289fb89275ec88a0d8b0ffeb64ffd5a4fe36cac7
Neill Payne <npayne@g-host.co.za>
2026-07-24 13:12:42 +0200

    Export VSCS project models

---
+++ src/vscs/domain/projects/__init__.py
@@ -0,0 +1,5 @@
+"""VSCS project domain models."""
+
+from vscs.domain.projects.models import ProductionSettings, ProjectMetadata, ProjectPaths
+
+__all__ = ["ProductionSettings", "ProjectMetadata", "ProjectPaths"]
