fd17ffde8ac0e36cb906df29279925bdcb424afe
Neill Payne <npayne@g-host.co.za>
2026-07-24 14:30:43 +0200

    Export Task 9 asset domain models

---
+++ src/vscs/domain/assets/__init__.py
@@ -0,0 +1,5 @@
+"""Production asset domain models."""
+
+from vscs.domain.assets.models import Asset, AssetCategory, AssetCreate, AssetStatus, AssetUpdate
+
+__all__ = ["Asset", "AssetCategory", "AssetCreate", "AssetStatus", "AssetUpdate"]
