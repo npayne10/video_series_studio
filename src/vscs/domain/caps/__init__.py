4a787e981a8cbb6ec01e32d6c7895a898ac3f09b
Neill Payne <npayne@g-host.co.za>
2026-07-24 16:01:57 +0200

    Export Task 10 CAP domain models

---
+++ src/vscs/domain/caps/__init__.py
@@ -0,0 +1,5 @@
+"""Canonical Asset Profile domain exports."""
+
+from vscs.domain.caps.models import CAPCreate, CAPStatus, CAPUpdate, CanonicalAssetProfile
+
+__all__ = ("CAPCreate", "CAPStatus", "CAPUpdate", "CanonicalAssetProfile")
