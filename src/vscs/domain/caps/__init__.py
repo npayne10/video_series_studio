51cd4f14bc349bb92fa55ae8266b459ed11214be
Neill Payne <npayne@g-host.co.za>
2026-07-25 12:11:28 +0200

    Export CAP intelligence domain models

--- src/vscs/domain/caps/__init__.py
+++ src/vscs/domain/caps/__init__.py
@@ -1,13 +1,24 @@
 """Canonical Asset Profile domain exports."""
 
-from vscs.domain.caps.generation import CAPGenerationRequest, GeneratedCAPDraft
+from vscs.domain.caps.generation import (
+    CAPCanonAnalysis,
+    CAPGenerationRequest,
+    CAPSectionConfidence,
+    CanonicalFactExtraction,
+    ExtractedCanonicalFact,
+    GeneratedCAPDraft,
+)
 from vscs.domain.caps.models import CanonicalAssetProfile, CAPCreate, CAPStatus, CAPUpdate
 
 __all__ = (
+    "CAPCanonAnalysis",
     "CAPCreate",
     "CAPGenerationRequest",
+    "CAPSectionConfidence",
     "CAPStatus",
     "CAPUpdate",
     "CanonicalAssetProfile",
+    "CanonicalFactExtraction",
+    "ExtractedCanonicalFact",
     "GeneratedCAPDraft",
 )
