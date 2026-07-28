705e7a3db4abff3b41e85edf0e3d6a751af94bfa
Neill Payne <npayne@g-host.co.za>
2026-07-28 15:56:37 +0200

    Export CAIE v2 knowledge base

--- src/vscs/application/caie/__init__.py
+++ src/vscs/application/caie/__init__.py
@@ -1,11 +1,21 @@
 """Canonical Asset Intelligence Engine exports."""
 
 from vscs.application.caie.engine import CAIEError, CanonicalAssetIntelligenceEngine
+from vscs.application.caie.knowledge_base import (
+    CAIEKnowledgeBase,
+    CAIEKnowledgeError,
+    DesignKnowledge,
+    StyleKnowledge,
+)
 from vscs.application.caie.models import CanonicalPromptContext, CanonicalPromptPackage
 
 __all__ = [
     "CAIEError",
+    "CAIEKnowledgeBase",
+    "CAIEKnowledgeError",
     "CanonicalAssetIntelligenceEngine",
     "CanonicalPromptContext",
     "CanonicalPromptPackage",
+    "DesignKnowledge",
+    "StyleKnowledge",
 ]
