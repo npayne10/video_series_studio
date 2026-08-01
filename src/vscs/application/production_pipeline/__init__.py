a48b88da546a66bd52f9ac33c8379fdea662c461
Neill Payne <npayne@g-host.co.za>
2026-08-01 17:10:11 +0200

    Expose render queue engine API

--- src/vscs/application/production_pipeline/__init__.py
+++ src/vscs/application/production_pipeline/__init__.py
@@ -2,6 +2,21 @@
 
 from .graph import ProductionGraph, ProductionGraphError
 from .models import ProductionNode, ProductionPipeline, ProductionStage, ProductionState
+from .queue import RenderQueueEngine, RenderQueueError
+from .queue_models import (
+    QueueAttempt,
+    QueuePriority,
+    QueueState,
+    RenderQueue,
+    RenderQueueEntry,
+)
+from .queue_serialization import RenderQueueSerializationError, RenderQueueSerializer
+from .queue_validator import (
+    QueueValidationIssue,
+    QueueValidationResult,
+    QueueValidationSeverity,
+    RenderQueueValidator,
+)
 from .serialization import (
     ProductionPipelineSerializationError,
     ProductionPipelineSerializer,
@@ -26,4 +41,17 @@ __all__ = [
     "ProductionPipelineValidator",
     "ProductionStage",
     "ProductionState",
+    "QueueAttempt",
+    "QueuePriority",
+    "QueueState",
+    "QueueValidationIssue",
+    "QueueValidationResult",
+    "QueueValidationSeverity",
+    "RenderQueue",
+    "RenderQueueEngine",
+    "RenderQueueEntry",
+    "RenderQueueError",
+    "RenderQueueSerializationError",
+    "RenderQueueSerializer",
+    "RenderQueueValidator",
 ]
