b00533f2cc44b06bf954e9866e325d1ffadcc8fa
Neill Payne <npayne@g-host.co.za>
2026-08-03 20:06:20 +0200

    Expose Story Browser v2 hierarchy API

--- src/vscs/application/story/__init__.py
+++ src/vscs/application/story/__init__.py
@@ -6,13 +6,29 @@ from .containers import (
     infer_container_type,
     normalize_container_id,
 )
+from .hierarchy import (
+    StoryHierarchy,
+    StoryItemStatus,
+    StoryNode,
+    StoryNodeKind,
+    StoryStatistics,
+    build_story_hierarchy,
+    scene_status,
+)
 from .service import StoryService, StoryServiceError
 
 __all__ = [
     "ProductionContainerType",
+    "StoryHierarchy",
+    "StoryItemStatus",
+    "StoryNode",
+    "StoryNodeKind",
     "StoryService",
     "StoryServiceError",
+    "StoryStatistics",
     "build_scene_id",
+    "build_story_hierarchy",
     "infer_container_type",
     "normalize_container_id",
+    "scene_status",
 ]
