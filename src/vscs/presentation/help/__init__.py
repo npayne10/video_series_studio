6ea48859aef5d9c185169301271ab1ee01e29515
Neill Payne <npayne@g-host.co.za>
2026-08-03 09:14:03 +0200

    Expose live VKF documentation panel

--- src/vscs/presentation/help/__init__.py
+++ src/vscs/presentation/help/__init__.py
@@ -2,6 +2,7 @@
 
 from .help_button import KnowledgeHelpButton
 from .help_popup import KnowledgeHelpPopup
+from .knowledge_documentation_panel import KnowledgeDocumentationPanel
 from .knowledge_provider import KnowledgeBinding, KnowledgeProvider
 from .knowledge_registry import (
     KnowledgeRegistry,
@@ -14,6 +15,7 @@ from .workflow_hint import WorkflowHint
 __all__ = [
     "SCENE_TOPICS",
     "KnowledgeBinding",
+    "KnowledgeDocumentationPanel",
     "KnowledgeHelpButton",
     "KnowledgeHelpPopup",
     "KnowledgeProvider",
