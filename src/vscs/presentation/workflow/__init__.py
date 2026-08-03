0636d6eacb437cc9fa9cdd1e7bfbdcb723369216
Neill Payne <npayne@g-host.co.za>
2026-08-03 12:34:23 +0200

    Expose guided workflow navigator

--- src/vscs/presentation/workflow/__init__.py
+++ src/vscs/presentation/workflow/__init__.py
@@ -1,10 +1,12 @@
 """Reusable guided-workflow presentation components."""
 
+from .guided_navigation import WorkflowNavigator
 from .workflow_progress import WorkflowProgressChecklist
 from .workflow_steps import SCENE_WORKFLOW_STEPS, WorkflowStep, WorkflowStepState
 
 __all__ = [
     "SCENE_WORKFLOW_STEPS",
+    "WorkflowNavigator",
     "WorkflowProgressChecklist",
     "WorkflowStep",
     "WorkflowStepState",
