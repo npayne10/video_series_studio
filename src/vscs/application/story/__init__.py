20bf3b7e5fd922916d2fcf1b8a711c87ffb1beec
Neill Payne <npayne@g-host.co.za>
2026-08-03 10:11:47 +0200

    Expose production container identities

--- src/vscs/application/story/__init__.py
+++ src/vscs/application/story/__init__.py
@@ -1,5 +1,18 @@
 """Structured story application services."""
 
+from .containers import (
+    ProductionContainerType,
+    build_scene_id,
+    infer_container_type,
+    normalize_container_id,
+)
 from .service import StoryService, StoryServiceError
 
-__all__ = ["StoryService", "StoryServiceError"]
+__all__ = [
+    "ProductionContainerType",
+    "StoryService",
+    "StoryServiceError",
+    "build_scene_id",
+    "infer_container_type",
+    "normalize_container_id",
+]
