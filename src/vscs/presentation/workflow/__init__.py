50598c8d6d495a072b485c935a3e789fcce70b8e
npayne10 <npayne@g-host.co.za>
2026-08-03 13:30:36 +0200

    Fix Ruff spacing in production audit tests

--- src/vscs/presentation/workflow/__init__.py
+++ src/vscs/presentation/workflow/__init__.py
@@ -6,8 +6,8 @@ from .workflow_progress import WorkflowProgressChecklist
 from .workflow_steps import SCENE_WORKFLOW_STEPS, WorkflowStep, WorkflowStepState
 
 __all__ = [
-    "CollapsibleWorkspacePanel",
     "SCENE_WORKFLOW_STEPS",
+    "CollapsibleWorkspacePanel",
     "WorkflowNavigator",
     "WorkflowProgressChecklist",
     "WorkflowStep",
