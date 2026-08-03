81667cb82d4d20df1e013bf6cb239c51fac9b6ea
Neill Payne <npayne@g-host.co.za>
2026-08-03 13:23:08 +0200

    Expose adaptive workspace panels

--- src/vscs/presentation/workflow/__init__.py
+++ src/vscs/presentation/workflow/__init__.py
@@ -1,10 +1,12 @@
 """Reusable guided-workflow presentation components."""
 
+from .adaptive_workspace import CollapsibleWorkspacePanel
 from .guided_navigation import WorkflowNavigator
 from .workflow_progress import WorkflowProgressChecklist
 from .workflow_steps import SCENE_WORKFLOW_STEPS, WorkflowStep, WorkflowStepState
 
 __all__ = [
+    "CollapsibleWorkspacePanel",
     "SCENE_WORKFLOW_STEPS",
     "WorkflowNavigator",
     "WorkflowProgressChecklist",
