10cb34f5f50027b94961ed22ab20d614107121c2
Neill Payne <npayne@g-host.co.za>
2026-08-03 13:42:07 +0200

    Expose Beginner Mode preference controller

--- src/vscs/presentation/workflow/__init__.py
+++ src/vscs/presentation/workflow/__init__.py
@@ -1,12 +1,14 @@
 """Reusable guided-workflow presentation components."""
 
 from .adaptive_workspace import CollapsibleWorkspacePanel
+from .beginner_mode import BeginnerModeController
 from .guided_navigation import WorkflowNavigator
 from .workflow_progress import WorkflowProgressChecklist
 from .workflow_steps import SCENE_WORKFLOW_STEPS, WorkflowStep, WorkflowStepState
 
 __all__ = [
     "SCENE_WORKFLOW_STEPS",
+    "BeginnerModeController",
     "CollapsibleWorkspacePanel",
     "WorkflowNavigator",
     "WorkflowProgressChecklist",
