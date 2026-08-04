94ffa4eba6f6faec41f29af04fcdb662fb060ff7
Neill Payne <npayne@g-host.co.za>
2026-08-04 15:16:48 +0200

    Export prompt graph builder contracts

--- src/vscs/application/prompt_graph/__init__.py
+++ src/vscs/application/prompt_graph/__init__.py
@@ -1,5 +1,13 @@
 """Renderer-neutral prompt graph production knowledge contracts."""
 
+from .builder import PromptGraphBuilder, PromptGraphBuildResult
+from .context import PromptGraphBuildContext
+from .diagnostics import (
+    PromptGraphBuildReport,
+    PromptGraphDiagnostic,
+    PromptGraphDiagnosticSeverity,
+    PromptGraphDiagnosticsFactory,
+)
 from .models import (
     PromptEdge,
     PromptEdgeKind,
@@ -10,17 +18,27 @@ from .models import (
     PromptNodeKind,
 )
 from .registry import PromptGraphRegistry, PromptGraphSnapshotRegistry
+from .resolver import PromptGraphResolver, PromptGraphSource
 from .snapshot import PromptGraphSnapshot, graph_checksum
 
 __all__ = [
     "PromptEdge",
     "PromptEdgeKind",
     "PromptGraph",
+    "PromptGraphBuilder",
+    "PromptGraphBuildContext",
+    "PromptGraphBuildReport",
+    "PromptGraphBuildResult",
     "PromptGraphCycleError",
+    "PromptGraphDiagnostic",
+    "PromptGraphDiagnosticSeverity",
+    "PromptGraphDiagnosticsFactory",
     "PromptGraphMetadata",
     "PromptGraphRegistry",
+    "PromptGraphResolver",
     "PromptGraphSnapshot",
     "PromptGraphSnapshotRegistry",
+    "PromptGraphSource",
     "PromptNode",
     "PromptNodeKind",
     "graph_checksum",
