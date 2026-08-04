55e0fab7c1a37ad11593900c9aa309eb558c85dc
Neill Payne <npayne@g-host.co.za>
2026-08-04 13:02:28 +0200

    Expose render request contracts

---
+++ src/vscs/application/rendering/requests.py
@@ -0,0 +1,19 @@
+"""Public render request contract aliases."""
+
+from .models import (
+    AssetPackageReference,
+    ContinuityPackageReference,
+    OutputSettings,
+    PromptPackageReference,
+    RenderRequest,
+    RenderSettings,
+)
+
+__all__ = [
+    "AssetPackageReference",
+    "ContinuityPackageReference",
+    "OutputSettings",
+    "PromptPackageReference",
+    "RenderRequest",
+    "RenderSettings",
+]
