8e18593cd2b2219ac1eaef276e4735648738623d
Neill Payne <npayne@g-host.co.za>
2026-08-07 14:45:39 +0200

    Export AI story analysis services and stage

--- src/vscs/application/story_analysis/__init__.py
+++ src/vscs/application/story_analysis/__init__.py
@@ -1,5 +1,13 @@
 """Public API for the VSCS story-analysis framework."""
 
+from vscs.application.story_analysis.ai_analysis import (
+    AssetServiceStoryEntityCatalog,
+    EmptyStoryEntityCatalog,
+    EntityResolutionService,
+    ExistingAssetReference,
+    StoryAIAnalysisProvider,
+    StoryEntityCatalog,
+)
 from vscs.application.story_analysis.bootstrap import register_story_analysis
 from vscs.application.story_analysis.contracts import (
     AnalysisContext,
@@ -20,10 +28,12 @@ from vscs.application.story_analysis.engine import (
 from vscs.application.story_analysis.knowledge_graph import StoryKnowledgeGraphBuilder
 from vscs.application.story_analysis.pipeline import StoryAnalysisPipeline
 from vscs.application.story_analysis.registry import StoryAnalysisStageRegistry
-from vscs.application.story_analysis.source_reader import StorySourceReader, StorySourceReadError
+from vscs.application.story_analysis.source_reader import StorySourceReadError, StorySourceReader
 from vscs.application.story_analysis.stages import (
+    AI_ENTITY_RESOLUTION_ARTIFACT,
     ANALYSIS_RESULT_ARTIFACT,
     KNOWLEDGE_GRAPH_ARTIFACT,
+    AIStoryAnalysisStage,
     StoryAnalysisEngineStage,
     StoryKnowledgeGraphStage,
 )
@@ -31,13 +41,20 @@ from vscs.application.story_analysis.stages import (
 STORY_KNOWLEDGE_GRAPH_ARTIFACT = KNOWLEDGE_GRAPH_ARTIFACT
 
 __all__ = [
+    "AI_ENTITY_RESOLUTION_ARTIFACT",
     "ANALYSIS_RESULT_ARTIFACT",
     "KNOWLEDGE_GRAPH_ARTIFACT",
     "STORY_KNOWLEDGE_GRAPH_ARTIFACT",
+    "AIStoryAnalysisStage",
     "AnalysisContext",
     "AnalysisStatus",
+    "AssetServiceStoryEntityCatalog",
     "DeterministicStoryAnalyzer",
+    "EmptyStoryEntityCatalog",
+    "EntityResolutionService",
+    "ExistingAssetReference",
     "StageResult",
+    "StoryAIAnalysisProvider",
     "StoryAnalysisEngine",
     "StoryAnalysisEngineStage",
     "StoryAnalysisPipeline",
@@ -45,6 +62,7 @@ __all__ = [
     "StoryAnalysisRequest",
     "StoryAnalysisStage",
     "StoryAnalysisStageRegistry",
+    "StoryEntityCatalog",
     "StoryKnowledgeGraphBuilder",
     "StoryKnowledgeGraphStage",
     "StorySection",
