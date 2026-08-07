5e703e2dfeb7c2ee922e62a13f1b882827915fc0
Neill Payne <npayne@g-host.co.za>
2026-08-07 13:07:06 +0200

    Export Phase 18.2.3 analysis engine API

--- src/vscs/application/story_analysis/__init__.py
+++ src/vscs/application/story_analysis/__init__.py
@@ -10,18 +10,36 @@ from vscs.application.story_analysis.contracts import (
     StoryAnalysisRequest,
     StoryAnalysisStage,
 )
+from vscs.application.story_analysis.engine import (
+    DeterministicStoryAnalyzer,
+    StorySection,
+    StoryStructureParser,
+    StoryTokenizer,
+    TextSpan,
+)
 from vscs.application.story_analysis.pipeline import StoryAnalysisPipeline
 from vscs.application.story_analysis.registry import StoryAnalysisStageRegistry
+from vscs.application.story_analysis.stages import (
+    ANALYSIS_RESULT_ARTIFACT,
+    StoryAnalysisEngineStage,
+)
 
 __all__ = [
+    "ANALYSIS_RESULT_ARTIFACT",
     "AnalysisContext",
     "AnalysisStatus",
+    "DeterministicStoryAnalyzer",
     "StageResult",
     "StoryAnalysisEngine",
+    "StoryAnalysisEngineStage",
     "StoryAnalysisPipeline",
     "StoryAnalysisReport",
     "StoryAnalysisRequest",
     "StoryAnalysisStage",
     "StoryAnalysisStageRegistry",
+    "StorySection",
+    "StoryStructureParser",
+    "StoryTokenizer",
+    "TextSpan",
     "register_story_analysis",
 ]
