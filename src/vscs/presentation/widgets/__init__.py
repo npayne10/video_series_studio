f4ba26128698f48a9161bbe5835251b1f8d894c5
Neill Payne <npayne@g-host.co.za>
2026-08-07 14:06:03 +0200

    Export Story Analysis workspace widgets

---
+++ src/vscs/presentation/widgets/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable VSCS presentation widgets."""
+
+from .story_analysis_workspace import StoryAnalysisWorkspaceDialog, StoryGraphView
+
+__all__ = ["StoryAnalysisWorkspaceDialog", "StoryGraphView"]
