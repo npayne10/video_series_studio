9fc9fdf3fcdad762439a4b9d2d41e9a8bd08078f
Neill Payne <npayne@g-host.co.za>
2026-08-10 12:32:54 +0200

    Export Behaviour Profile governance service

--- src/vscs/application/behaviours/__init__.py
+++ src/vscs/application/behaviours/__init__.py
@@ -1,11 +1,21 @@
-"""Behaviour Profile application persistence contracts."""
+"""Behaviour Profile application persistence and governance contracts."""
 
 from vscs.application.behaviours.repository import (
     BehaviourProfileRepository,
     BehaviourProfileRepositoryError,
 )
+from vscs.application.behaviours.service import (
+    BehaviourGovernanceError,
+    BehaviourProfileNotFoundError,
+    BehaviourProfileService,
+    BehaviourProfileServiceError,
+)
 
 __all__ = [
+    "BehaviourGovernanceError",
+    "BehaviourProfileNotFoundError",
     "BehaviourProfileRepository",
     "BehaviourProfileRepositoryError",
+    "BehaviourProfileService",
+    "BehaviourProfileServiceError",
 ]
