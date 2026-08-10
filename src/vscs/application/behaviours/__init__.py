7036d32f21c652baffb5d4b9da16a0cd13d74a99
Neill Payne <npayne@g-host.co.za>
2026-08-10 11:51:25 +0200

    Add Behaviour Profile application repository package

---
+++ src/vscs/application/behaviours/__init__.py
@@ -0,0 +1,11 @@
+"""Behaviour Profile application persistence contracts."""
+
+from vscs.application.behaviours.repository import (
+    BehaviourProfileRepository,
+    BehaviourProfileRepositoryError,
+)
+
+__all__ = [
+    "BehaviourProfileRepository",
+    "BehaviourProfileRepositoryError",
+]
