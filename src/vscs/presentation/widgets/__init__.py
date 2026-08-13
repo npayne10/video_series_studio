76e29372fdc0f4a64452dce99f9a4b3ec41e79d5
Neill Payne <npayne@g-host.co.za>
2026-08-13 13:49:19 +0200

    Install Production Planning performance refinement

--- src/vscs/presentation/widgets/__init__.py
+++ src/vscs/presentation/widgets/__init__.py
@@ -1,7 +1,10 @@
 """Reusable VSCS presentation widgets."""
 
+from .production_planning_performance import install_production_planning_performance
 from .story_analysis_workspace import StoryAnalysisWorkspaceDialog, StoryGraphView
 from .story_intelligence_dashboard import StoryIntelligenceDashboardDialog
+
+install_production_planning_performance()
 
 __all__ = [
     "StoryAnalysisWorkspaceDialog",
