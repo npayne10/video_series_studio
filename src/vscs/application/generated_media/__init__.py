9e60da056282ea832cfb9619b1131874c7dcd6c0
Neill Payne <npayne@g-host.co.za>
2026-08-18 15:48:07 +0200

    Export Phase 20.1 Generated Media governance

---
+++ src/vscs/application/generated_media/__init__.py
@@ -0,0 +1,17 @@
+"""Generated Media application services for VSCS Phase 20."""
+
+from .governance import (
+    GeneratedMediaGovernanceError,
+    GeneratedMediaGovernanceIssue,
+    GeneratedMediaGovernanceResult,
+    GeneratedMediaGovernanceService,
+    GeneratedMediaGovernanceSeverity,
+)
+
+__all__ = [
+    "GeneratedMediaGovernanceError",
+    "GeneratedMediaGovernanceIssue",
+    "GeneratedMediaGovernanceResult",
+    "GeneratedMediaGovernanceService",
+    "GeneratedMediaGovernanceSeverity",
+]
