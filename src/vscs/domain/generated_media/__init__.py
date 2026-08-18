b85ed6f691a6329aa4081c6a2e9806b79735c1e9
Neill Payne <npayne@g-host.co.za>
2026-08-18 15:47:20 +0200

    Export Phase 20.1 Generated Media domain

---
+++ src/vscs/domain/generated_media/__init__.py
@@ -0,0 +1,21 @@
+"""Authoritative Generated Media domain for VSCS Phase 20."""
+
+from .models import (
+    GeneratedMedia,
+    GeneratedMediaFile,
+    GeneratedMediaGovernanceEvent,
+    GeneratedMediaKind,
+    GeneratedMediaProvenance,
+    GeneratedMediaScope,
+    GeneratedMediaState,
+)
+
+__all__ = [
+    "GeneratedMedia",
+    "GeneratedMediaFile",
+    "GeneratedMediaGovernanceEvent",
+    "GeneratedMediaKind",
+    "GeneratedMediaProvenance",
+    "GeneratedMediaScope",
+    "GeneratedMediaState",
+]
