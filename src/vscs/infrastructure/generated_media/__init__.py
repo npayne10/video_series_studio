3ee5f9b77432a4fec91ab49a6b9ebe0d12bb5bb8
Neill Payne <npayne@g-host.co.za>
2026-08-19 15:40:16 +0200

    Export FFprobe Generated Media inspector

--- src/vscs/infrastructure/generated_media/__init__.py
+++ src/vscs/infrastructure/generated_media/__init__.py
@@ -1,6 +1,12 @@
 """Generated Media infrastructure adapters."""
 
+from .ffprobe_inspector import FFprobeGeneratedMediaInspector, SubprocessFFprobeRunner
 from .file_store import LocalGeneratedMediaFileStore
 from .repository import JsonGeneratedMediaRepository
 
-__all__ = ["JsonGeneratedMediaRepository", "LocalGeneratedMediaFileStore"]
+__all__ = [
+    "FFprobeGeneratedMediaInspector",
+    "JsonGeneratedMediaRepository",
+    "LocalGeneratedMediaFileStore",
+    "SubprocessFFprobeRunner",
+]
