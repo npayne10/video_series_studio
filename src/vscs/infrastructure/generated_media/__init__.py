fbd4eed824c1e98748dc6f5adf6c76b1680de0da
Neill Payne <npayne@g-host.co.za>
2026-08-19 16:03:32 +0200

    Export Phase 20.12 selection persistence

--- src/vscs/infrastructure/generated_media/__init__.py
+++ src/vscs/infrastructure/generated_media/__init__.py
@@ -3,10 +3,12 @@
 from .ffprobe_inspector import FFprobeGeneratedMediaInspector, SubprocessFFprobeRunner
 from .file_store import LocalGeneratedMediaFileStore
 from .repository import JsonGeneratedMediaRepository
+from .selection_repository import JsonGeneratedMediaSelectionRepository
 
 __all__ = [
     "FFprobeGeneratedMediaInspector",
     "JsonGeneratedMediaRepository",
+    "JsonGeneratedMediaSelectionRepository",
     "LocalGeneratedMediaFileStore",
     "SubprocessFFprobeRunner",
 ]
