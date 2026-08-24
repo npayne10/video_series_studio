e66889cb79c150f32c47c1815e6b7889e068d191
Neill Payne <npayne@g-host.co.za>
2026-08-24 12:44:07 +0200

    Export LTX 2.3 validation pack

--- src/vscs/infrastructure/provider_capability_validation/__init__.py
+++ src/vscs/infrastructure/provider_capability_validation/__init__.py
@@ -1,6 +1,11 @@
 """Provider capability-validation infrastructure exports."""
 
+from .ltx23 import ltx23_video_validation_pack
 from .repository import JsonCapabilityValidationRepository
 from .wan22 import wan22_video_validation_pack
 
-__all__ = ["JsonCapabilityValidationRepository", "wan22_video_validation_pack"]
+__all__ = [
+    "JsonCapabilityValidationRepository",
+    "ltx23_video_validation_pack",
+    "wan22_video_validation_pack",
+]
