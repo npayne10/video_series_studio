561f728e7915bca54963ca5f3fedb5c3fc62f238
Neill Payne <npayne@g-host.co.za>
2026-08-27 13:50:21 +0200

    Export LTX 2.3 Video Studio integration

--- src/vscs/infrastructure/rendering/__init__.py
+++ src/vscs/infrastructure/rendering/__init__.py
@@ -16,6 +16,13 @@ from .comfyui_live import (
     UrllibComfyUITransport,
 )
 from .comfyui_production import ProductionPackageComfyUIAdapter
+from .ltx23_video_studio import (
+    LTX23_VIDEO_STUDIO_DISPLAY_NAME,
+    LTX23_VIDEO_STUDIO_WORKFLOW_ID,
+    LTX23VideoStudioDeploymentValidator,
+    LTX23VideoStudioInputResolver,
+    build_ltx23_video_studio_foundation,
+)
 
 __all__ = [
     "ComfyUIAdapter",
@@ -26,8 +33,13 @@ __all__ = [
     "ComfyUILiveAdapterError",
     "ComfyUITransport",
     "ComfyUIWorkflowCompiler",
+    "LTX23_VIDEO_STUDIO_DISPLAY_NAME",
+    "LTX23_VIDEO_STUDIO_WORKFLOW_ID",
+    "LTX23VideoStudioDeploymentValidator",
+    "LTX23VideoStudioInputResolver",
     "LiveComfyUIAdapter",
     "MetadataComfyUIInputResolver",
     "ProductionPackageComfyUIAdapter",
     "UrllibComfyUITransport",
+    "build_ltx23_video_studio_foundation",
 ]
